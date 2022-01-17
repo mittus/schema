44299aabda83e8c6413d7e034da6707b85bca046
Dmitry Kolesnikov <mittus@ya.ru>
2022-01-17 10:16:11 +0300

    add massage seat type, optimizate scheme-view, hide img preload

--- hall-schema-servise.js
+++ hall-schema-servise.js
@@ -16,7 +16,7 @@ class HallSchemaService {
 			xhr.onerror = function() {
 				reject('Error while getting XML.');
 			}
-			xhr.open('GET', `/performance.json?get_performance=${schemeId}`);
+			xhr.open('GET', 'https://pay.lumenfilm.ru/api/kinokassa/?get_performance='+schemeId);
 			xhr.responseType = 'json';
 			xhr.send();
 		});
--- helpers.js
+++ helpers.js
@@ -86,3 +86,8 @@ function getHasIntersection(rect1, rect2) {
 		&& ((rect1.y1 - rect2.y1) * (rect1.y2 - rect2.y2) < 0)
 	);
 }
+
+function shadeColor(color, percent) {  // deprecated. See below.
+    var num = parseInt(color.slice(1),16), amt = Math.round(2.55 * percent), R = (num >> 16) + amt, G = (num >> 8 & 0x00FF) + amt, B = (num & 0x0000FF) + amt;
+    return "#" + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (G<255?G<1?0:G:255)*0x100 + (B<255?B<1?0:B:255)).toString(16).slice(1);
+}
